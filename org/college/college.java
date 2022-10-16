7f6685d6f3e20b8fdafecde6346f4f4e5fb4e13c
HemashaliniBabu <105559791+HemashaliniBabu@users.noreply.github.com>
2022-10-16 12:28:39 +0530

    Add files via upload

---
+++ org/college/college.java
@@ -0,0 +1,19 @@
+package wee3day1.org.college;
+
+public class college {
+
+public void collegeName() {
+		
+		System.out.println("College class --> College Name: ABC INSTITUTE OF TECHNOLOGY");
+	}
+	
+	public void collegeCode() {
+		
+		System.out.println("College class -->College Code: 1206");
+	}
+	
+	public void collegeRank() {
+		
+		System.out.println("College class -->College Rank: 1");
+	}
+}

3b4ce548aff4331d78f3b2674b53ef71fb95c9fa
HemashaliniBabu <105559791+HemashaliniBabu@users.noreply.github.com>
2022-10-16 12:30:42 +0530

    Add files via upload

---
+++ org/college/department.java
@@ -0,0 +1,12 @@
+package week3day1.org.department;
+
+import wee3day1.org.college.college;
+
+public class department extends college {
+	
+	public void deptName() {
+		
+		System.out.println("Department class--> Department: CSE");
+	}
+	
+}
